43c4aa527868b7452a4a43e79c2954579d747864
Avijit Ash <avijitash06@gmail.com>
2025-06-23 18:33:57 +0530

    create task 1 landing page navbar

---
+++ Task 1-Landing page/script.js
@@ -0,0 +1,25 @@
+const category = document.getElementById("categoryBtn");
+const menu = document.getElementById("menu");
+let catFlag = false;
+category.addEventListener("click", function () {
+  if (catFlag === false) {
+    menu.style.display = "block";
+    catFlag = true;
+  } else {
+    menu.style.display = "none";
+    catFlag = false;
+  }
+});
+category.addEventListener("blur", function () {
+  menu.style.display = "none";
+  catFlag = false;
+});
+
+const search = document.getElementById("search");
+const searchContainer = document.getElementById("serachContainer");
+search.addEventListener("focus", function () {
+  searchContainer.style.border = "thin solid #388e3b";
+});
+search.addEventListener("blur", function () {
+  searchContainer.style.border = "";
+});
